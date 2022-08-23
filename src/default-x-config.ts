a0184c1c9184c6f8bc9e41716193bfb088a90d7e
Edwin <digiaries@hotmail.com>
2022-08-23 10:57:10 +0800

    fix: 修复配置不正常传递的问题

--- src/default-x-config.ts
+++ src/default-x-config.ts
@@ -3,17 +3,16 @@ import { isString } from "@x-drive/utils";
 /**默认业务目录 */
 var businessDir = "@server";
 
-const XConfig = Object.defineProperties(null, {
-    "business": {
-        get() {
-            return businessDir;
-        }
-        , set(val: string) {
-            if (isString(val)) {
-                businessDir = val;
-            }
+class Config {
+    get businessDir() {
+        return businessDir;
+    }
+
+    set businessDir(val: string) {
+        if (isString(val)) {
+            businessDir = val;
         }
     }
-});
+}
 
-export default XConfig;
+export default new Config;
