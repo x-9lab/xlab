01d90688841b383af19197ccfd968c7038bbce9d
Edwin <digiaries@hotmail.com>
2023-03-24 11:27:07 +0800

    feat: 去除与基本功能无关的内部组件, 这些组件将以外部组件的形式存在

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,32 +2,14 @@
 interface InternalComponents {
     /**资产 */
     assets: typeof import("./assets");
-    /**缓存 */
-    cache: typeof import("./cache");
     /**cluster */
     cluster: typeof import("./cluster");
     /**Http header 相关 */
     header: typeof import("./header");
-    /**Html 内容处理器 */
-    "html-processor": typeof import("./html-processor");
-    /**JS 内容处理器 */
-    "js-processor": typeof import("./js-processor");
-    /**数据注入 */
-    injection: typeof import("./injection");
-    /**MD5 */
-    md5: typeof import("./md5");
     /**MIME */
     mime: typeof import("./mime");
-    /**客户端判断 */
-    platform: typeof import("./platform");
-    /**请求参数处理 */
-    querystring: typeof import("./querystring");
-    /**各种重定向方法 */
-    redirect: typeof import("./redirect");
     /**简单的 uuid */
     uuid: typeof import("./uuid");
-    /**版本比对 */
-    version: typeof import("./version");
     /**定时任务 */
     cron: typeof import("./cron");
     /**日志 */
@@ -36,7 +18,5 @@ interface InternalComponents {
     "return-code": typeof import("./return-code");
     /**通用工具函数集 */
     common: typeof import("./common");
-    /**内部请求模块 */
-    request: typeof import("./request");
 }
 export type { InternalComponents }
--- src/components/md5/index.ts
+++
@@ -1,9 +0,0 @@
-import crypto from "crypto";
-
-function md5(str: string) {
-    var md5 = crypto.createHash("md5");
-    md5.update(str);
-    return md5.digest("hex");
-}
-
-export { md5 };
